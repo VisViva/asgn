6df5db3b24d52f37c4948b51758ce64c5f541dbb
Vis Viva <a.vis.viva@gmail.com>
2016-12-16 00:13:44 +0300

    Got rid of old math functions

--- src/utils/math.js
+++ src/utils/math.js
@@ -1,55 +1,6 @@
 'use strict';
 
 /**
- * Convert degrees to radians
- */
-
-export function deg_to_rad(degrees) {
-
-    if (degrees > 0 && degrees % 360 === 0) {
-        return trim_float(Math.PI * 2);
-    }
-    return trim_float(degrees % 360 * (Math.PI / 180));
-}
-
-/**
- * Convert degrees to radians
- */
-
-export function rad_to_deg(radians) {
-    let negative = radians < 0;
-    if (negative) {
-        return (~~((radians * (180 / Math.PI))) % 360);
-    }
-    return trim_float(Math.ceil((radians * (180 / Math.PI))) % 360);
-}
-
-/**
- * Trim floats to certain precision
- */
-
-export function trim_float(float, digits = 5) {
-    const trimmer = Math.pow(10, digits);
-    return Math.round(float * trimmer) / trimmer;
-}
-
-/**
- * Trim angles
- */
-
-export function trim_angle(angle) {
-    if (angle > 360) {
-        return angle % 360;
-    } else {
-        if (angle < 0) {
-            return 360 + angle % 360;
-        } else {
-            return angle;
-        }
-    }
-}
-
-/**
  * Circle hit test
  */
 
@@ -95,5 +46,3 @@ export function get_circle_radius_by_area(area) {
 export function get_center_of_mass_of_parallelogram(A, B, C, D) {
     return [(A[0] + B[0] + C[0] + D[0]) / 4, (A[1] + B[1] + C[1] + D[1]) / 4];
 }
-
-
