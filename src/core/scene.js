4e5d03f6a5e4eb8709f6b7292bfd2a78fbaf2beb
Vis Viva <a.vis.viva@gmail.com>
2016-12-15 10:20:55 +0300

    Scene clearing

--- src/core/scene.js
+++ src/core/scene.js
@@ -16,10 +16,7 @@ function Scene(container, name) {
     // 2d rendering context
     this._context = this._canvas.getContext('2d');
 
-    /**
-     * Resize the scene
-     */
-
+    // Resize the scene
     this.resize();
 };
 
@@ -35,7 +32,31 @@ Scene.prototype.resize = function() {
     this._canvas.width = this._canvas.offsetWidth;
     this._canvas.height = this._canvas.offsetHeight;
 
+    // Render the scene
+    this.render();
+
     return this;
+};
+
+/**
+ * Clear the context
+ */
+
+Scene.prototype.clear = function() {
+    
+    // Clear the context and reset the transformation matrix
+    this._context.setTransform(1, 0, 0, 1, 0, 0);
+    this._context.fillRect(0, 0, this._canvas.width, this._canvas.height);
+};
+
+/**
+ * Render the primitives
+ */
+
+Scene.prototype.render = function() {
+    
+    // Clear the context
+    this.clear();
 };
 
 exports.Scene = Scene;
